6165b194dd82a128a612d5a1bb71b2f1f1ce2ff5
Frazer Smith <2324003s@student.gla.ac.uk>
2020-03-06 16:27:23 +0000

    Model Creation v1

--- rango/models.py
+++ rango/models.py
@@ -15,14 +15,6 @@ class Film(models.Model):
     poster = models.ImageField()
 
 
-class Review(models.Model):
-    reviewerID = models.IntegerField()
-    filmID = models.IntegerField()
-    mainBody = models.CharField(max_length = 1000)
-    rating = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(5)])
-    date = models.DateTimeField()
-
-
 class Rating(models.Model):
     userID = models.IntegerField()
     filmID = models.IntegerField()
@@ -38,6 +30,13 @@ class Reviewer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
     displayName = models.CharField(max_length = 128)
     profilePicture = models.ImageField()
+
+class Review(models.Model):
+    reviewerID = models.ForeignKey(Reviewer, on_delete=models.CASCADE)
+    filmID = models.ForeignKey(Film, on_delete=models.CASCADE)
+    mainBody = models.CharField(max_length = 1000)
+    rating = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(5)])
+    date = models.DateTimeField()
 
 
 
